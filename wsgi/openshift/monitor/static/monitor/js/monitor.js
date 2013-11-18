7f4ae08a1aa4f6d145fcc3e0987ebdcedca04afe
Cameron Morrison <cameron.am@gmail.com>
2013-11-18 22:57:28 +1000

    trying to use rickshaw, need more good test data to make this work

--- wsgi/openshift/monitor/static/monitor/js/monitor.js
+++ wsgi/openshift/monitor/static/monitor/js/monitor.js
@@ -1,42 +1,64 @@
 
 $(function(){
-		$.get('http://127.0.0.1:8000/api/record/').done(function(response) {
-			console.log(this);
-			console.log(response);
+	$.get('http://127.0.0.1:8000/api/record/').done(function(response) {
+		// console.log(this);
+		console.log(response);
+
+		var chartLines = _.reduce(response.objects, function(graphLines, measurementSet, index){ 
+			// console.table(value);
+
+			// {
+			// 	key: "p1",
+			// 	values: [
+			// 		{  x: date, y: value  },
+			// 		{  x: date, y: value  }
+			// 	]
+			// }
+
+			var instant = moment(measurementSet.instant);
+			_.each(measurementSet.measurements, function(temperature, probe){
+				var graphLine = _.find(graphLines, { name: probe });
+
+				if(graphLine == null) {
+					graphLine = { name: probe, data: [] };
+					graphLines.push(graphLine);
+				}
+
+				graphLine.data.push({ x: instant.unix(), y: temperature });
+			});
+
+			return graphLines;
+		}, []);
+
+
+
+		var graph = new Rickshaw.Graph({
+		        element: document.querySelector("#chart"),
+		        width: 580,
+		        height: 250,
+		        series: [_.last(chartLines)]
 		});
 
+		var axes = new Rickshaw.Graph.Axis.Time( { graph: graph } );
 
-		return stream_layers(3,128,.1).map(function(data, i) {
-			return { 
-			  key: 'Stream' + i,
-			  values: data
-			};
-		});
+		graph.render();
 
 
+		// var chart = nv.models.lineWithFocusChart();
 
+		// // chart.transitionDuration(500);
+		// chart.xAxis.tickFormat(function(d) { 
+		// 	return d3.time.format('%Y-%m-%dT%H:%M:%S')(new Date(d)); 
+		// });
 
+		// chart.x2Axis.tickFormat(function(d) { 
+		// 	return d3.time.format('%Y-%m-%dT%H:%M:%S')(new Date(d)); 
+		// });
 
-	nv.addGraph(function() {
-	  var chart = nv.models.lineWithFocusChart();
+		// // chart.yAxis.tickFormat(d3.format(',.2f'));
 
-	 // chart.transitionDuration(500);
-	  chart.xAxis
-	      .tickFormat(d3.format(',f'));
-	  chart.x2Axis
-	      .tickFormat(d3.format(',f'));
+		// d3.select('#chart svg').datum(chartLines).call(chart);
 
-	  chart.yAxis
-	      .tickFormat(d3.format(',.2f'));
-	  chart.y2Axis
-	      .tickFormat(d3.format(',.2f'));
-
-	  d3.select('#chart svg')
-	      .datum(fetchData())
-	      .call(chart);
-
-	  nv.utils.windowResize(chart.update);
-
-	  return chart;
+		// nv.utils.windowResize(chart.update);
 	});
 });
